fd319d61c4c0207eb1dab6043111a89955652c00
Oluwaseun <oluwaseunadesina8@gmail.com>
2024-02-16 07:54:50 +0100

    Updating JWT Exiration date

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -50,7 +50,7 @@ const userController = {
                 return res.status(400).json({ message: 'Invalid credentials' });
             }
 
-            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '3h' });
             res.status(200).json({ message: 'Login successful', token });
         } catch (error) {
             res.status(500).json({ message: error.message });
